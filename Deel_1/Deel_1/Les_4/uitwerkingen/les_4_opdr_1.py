7becae1179f860149d108b5f117b21c689ab861d
YilmazNL2005 <99069863@mydavinci.nl>
2023-09-05 18:18:11 +0200

    les4_opdr1_en_2_completed

---
+++ Deel_1/Deel_1/Les_4/uitwerkingen/les_4_opdr_1.py
@@ -0,0 +1,7 @@
+print( 15+4 ) # er wordt 4 bij 15 opgeteld
+print( 15-4 ) # er wordt 4 van 15 afgehaald 
+print( 15*4 ) # 15 keer 4
+print( 15/4 ) # 15 wordt gedeeld over 4
+print( 15//4 ) # 15 wordt gedeeld over 4 en het komma getal wordt afgerond naar een heel getal
+print( 15**4 ) # 15 tot de macht 4
+print( 15%4 ) # 15 wordt gedeeld over 4 en het resterende getal wordt in de terminal getoond.
---
+++ Deel_1/Deel_1/Les_4/uitwerkingen/les_4_opdr_2.py
@@ -0,0 +1,8 @@
+print( 5*2 -3+4/2 )
+print( 5*2 - 3+4 / 2 )
+# Krijg je nu dezelfde uitkomst? Antwoord = ja
+
+print( (5*2) - (3+4) /2 ) # de sommen die tussen de haakjes staan worden eerst berekend, daarna komt de rest met de min en gedeeld door pas.
+print( ((5*2) -(3+4)) / 2 ) # de sommen die tussen de haakjes staan worden eerst berekend, daarvan de 2 uitkomsten worden van elkaar afgetrokken en tot slot wordt het gedeeld door 2.
+# Vreemd, dezelfde getallen. Kun je de uitkomst verklaren? 
+
