c846b635745a9ab03ae13af5c4accb62a75c8ae0
YilmazNL2005 <99069863@mydavinci.nl>
2023-09-08 10:25:36 +0200

    gewerkt_aan_les9

---
+++ Deel_1/Les_9/uitwerkingen/les_9_opdr_2.py
@@ -0,0 +1,9 @@
+a = int(input("Vul een getal in. "))
+b = int(input("Vul een getal in. "))
+
+
+berekening = 11%7
+if berekening != 0:
+    print(berekening)
+else:
+    print("De berekening heeft geen rest.")
