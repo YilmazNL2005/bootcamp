74285989b8c5b9b290a71d35549825fea9fef4a3
YilmazNL2005 <99069863@mydavinci.nl>
2023-09-11 12:57:05 +0200

    Alles_completed

--- Deel_1/Les_9/uitwerkingen/les_9_opdr_3.py
+++ Deel_1/Les_9/uitwerkingen/les_9_opdr_3.py
@@ -1,6 +1,5 @@
 leeftijd = int(input("Wat is je leeftijd?"))
 snor = input("Heb je een snor? ja/nee ")
 diploma = input("Heb je een diploma? ja/nee ")
-if leeftijd >= 18:
-    ##
-    ##
+if leeftijd >= 18 and snor == "ja" or diploma == "ja" and leeftijd < 18:
+    print("Je bent aangenomen!!!")
