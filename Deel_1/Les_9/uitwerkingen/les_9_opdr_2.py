b12b6a2bcd002fd3078bf955e428bc9d4bac0d4e
YilmazNL2005 <99069863@mydavinci.nl>
2023-09-11 12:55:19 +0200

    Les_9_verbeterd

--- Deel_1/Les_9/uitwerkingen/les_9_opdr_2.py
+++ Deel_1/Les_9/uitwerkingen/les_9_opdr_2.py
@@ -1,9 +1,17 @@
+# a = int(input("Vul een getal in. "))
+# b = 7
+# c = 11
+# berekening_ab = b%a
+# berekening_ac = c%a
+# if berekening_ab != 0 and berekening_ac:
+#     print(f"het getal {b} en {c} zijn niet zonder rest deelbaar")
+# else:
+#     print(f" {a} past zonder rest in {b} en {c} de heeft geen rest.")
+
 a = int(input("Vul een getal in. "))
-b = int(input("Vul een getal in. "))
-
-
-berekening = 11%7
-if berekening != 0:
-    print(berekening)
+b = 7
+c = 11
+if b%a != 0 and c%a:
+    print(f"het getal {b} en {c} zijn niet zonder rest deelbaar")
 else:
-    print("De berekening heeft geen rest.")
+    print(f" {a} past zonder rest in {b} en {c} de heeft geen rest.")
---
+++ Deel_1/Les_9/uitwerkingen/les_9_opdr_3.py
@@ -0,0 +1,6 @@
+leeftijd = int(input("Wat is je leeftijd?"))
+snor = input("Heb je een snor? ja/nee ")
+diploma = input("Heb je een diploma? ja/nee ")
+if leeftijd >= 18:
+    ##
+    ##
