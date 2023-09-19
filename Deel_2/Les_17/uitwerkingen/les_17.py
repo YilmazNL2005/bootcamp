8078e2cccb018eadee921a600cf69a0155715e7e
YilmazNL2005 <99069863@mydavinci.nl>
2023-09-19 11:39:21 +0200

    les_17_opdr_2

--- Deel_2/Les_17/uitwerkingen/les_17.py
+++ Deel_2/Les_17/uitwerkingen/les_17.py
@@ -1,8 +1,11 @@
 import random
-getal = random.randint(1,5)
-print(getal)
-raden = int(input("Geef een getal op tussen de 1 en 5. "))
-if getal == raden:
-    print("Je hebt het getal goed geraden!")
-else:
-    print("Je hebt het getal niet goed geraden!")
+
+while True:
+    getal = random.randint(1,5)
+    print(getal)
+    raden = int(input("Geef een getal op tussen de 1 en 5. "))
+    if getal == raden:
+        print("Je hebt het getal goed geraden!")
+        break
+    else:
+        print("Je hebt het getal niet goed geraden!")
