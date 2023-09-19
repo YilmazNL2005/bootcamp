d2cf980fd538f1756b4df3e75becef78d3da67fc
YilmazNL2005 <99069863@mydavinci.nl>
2023-09-19 11:41:30 +0200

    les_17_opdr_3

--- Deel_2/Les_17/uitwerkingen/les_17.py
+++ Deel_2/Les_17/uitwerkingen/les_17.py
@@ -1,11 +1,12 @@
 import random
+aantal_rondes = 0
 
-while True:
+while aantal_rondes < 3:
     getal = random.randint(1,5)
     print(getal)
     raden = int(input("Geef een getal op tussen de 1 en 5. "))
     if getal == raden:
         print("Je hebt het getal goed geraden!")
-        break
+        aantal_rondes += 1
     else:
         print("Je hebt het getal niet goed geraden!")
