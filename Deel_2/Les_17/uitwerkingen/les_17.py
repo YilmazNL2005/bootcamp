e6f1a0fb4f9a81b06f50e06cac92a20f7522ef2f
YilmazNL2005 <99069863@mydavinci.nl>
2023-09-19 11:50:39 +0200

    les_17_opdr_4

--- Deel_2/Les_17/uitwerkingen/les_17.py
+++ Deel_2/Les_17/uitwerkingen/les_17.py
@@ -1,12 +1,18 @@
 import random
-aantal_rondes = 0
-
-while aantal_rondes < 3:
+aantal_rondes = 1
+antwoorden_fout = 0
+doorgaan = "ja"
+while doorgaan == "ja":
     getal = random.randint(1,5)
     print(getal)
     raden = int(input("Geef een getal op tussen de 1 en 5. "))
     if getal == raden:
         print("Je hebt het getal goed geraden!")
-        aantal_rondes += 1
+        doorgaan = input("Wil je nog een ronde spelen? ja / nee ")
+        if doorgaan == "ja":
+            aantal_rondes += 1
+        else:
+            print(f"Je hebt {aantal_rondes} rondes gespeeld en {antwoorden_fout} foute antwoorden.")
     else:
         print("Je hebt het getal niet goed geraden!")
+        antwoorden_fout += 1
